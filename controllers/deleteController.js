4ada7aac9fae37cf5a5542c675499e5918369812
foxane <kafisaif45@gmail.com>
2024-10-25 20:57:33 +0700

    Add password valdation to delete item Resurrected error from deletion Delete edit.ejs Add button to create new item and category on their respective page

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -1,7 +1,12 @@
 import * as db from '../db/queries.js';
-import { InternalServerError } from '../helper/errors.js';
+import { CustomError, InternalServerError } from '../helper/errors.js';
+
+const pwValidation = pw => pw === 'vaasmontenegro';
 
 export const deleteItem = async (req, res, next) => {
+  if (!pwValidation(req.query.pw)) {
+    return next(new CustomError('Wrong password! Refer to source code', 401));
+  }
   try {
     const { itemId } = req.params;
     await db.deleteItem(Number(itemId));
@@ -13,6 +18,9 @@ export const deleteItem = async (req, res, next) => {
 };
 
 export const deleteCategory = async (req, res, next) => {
+  if (!pwValidation(req.query.pw)) {
+    return next(new CustomError('Wrong password! Refer to source code', 401));
+  }
   try {
     const { categoryId } = req.params;
     await db.deleteCategory(Number(categoryId));
--- routes/deleteRouter.js
+++ routes/deleteRouter.js
@@ -4,8 +4,8 @@ import { deleteCategory, deleteItem } from '../controllers/deleteController.js';
 
 const deleteRouter = Router();
 
-deleteRouter.get('/item/:itemId', deleteItem);
-deleteRouter.get('/category/:categoryId', deleteCategory);
+deleteRouter.post('/item/:itemId', deleteItem);
+deleteRouter.post('/category/:categoryId', deleteCategory);
 deleteRouter.get('/', (req, res, next) => {
   next(new NotFoundError());
 });
